1f330eb20af3c1d29d0b9033a3ab41a0c5005dba
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-04-16 19:56:24 +0900

    사운드 수정

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,21 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class AudioManager : MonoBehaviour
 {
-
-
-    public static AudioManager Instance;
-    
+    public static AudioManager instance;
     private AudioSource audioSource;
     [SerializeField] private AudioClip bgm;
-
     private void Awake()
     {
-        if (Instance == null)
+        if (instance == null)
         {
-            Instance = this;
+            instance = this;
             DontDestroyOnLoad(gameObject); // 씬을 이동해도 AudioManager가 파괴되지않음
         }
         else
@@ -25,31 +20,31 @@ public class AudioManager : MonoBehaviour
         }
         audioSource = GetComponent<AudioSource>();
     }
-
     private void Start()
     {
         BGM_On(bgm);
     }
-
-    
-    // 효율적인 코딩
     public void BGM_On(AudioClip bgm)
     {
-        // bgm을 플레이하는 코드
-        audioSource.clip = bgm;
-        audioSource.Play(); 
-    }
-    public void PlayUrgentMusic() // 긴박한 음악 재생 메서드 추가
-    {
-        audioSource.Stop(); // 배경 음악 중지
         audioSource.clip = bgm;
         audioSource.Play();
     }
-
-    public void StopUrgentMusic() // 긴박한 음악 중지 메서드 추가
+    public void SwitchBGM(AudioClip newBGM)
     {
         audioSource.Stop();
-        audioSource.clip = bgm;
-        audioSource.Play(); // 배경 음악 재생
+        BGM_On(newBGM);
+    }
+    //2024.04.16
+    public void StopBGM()
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+    }
+    // 2024.04.16
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,94 +3,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance;
-
+    public static GameManager instance ;
     public Card FirstCard { get; set; }
     public Card SecondCard { get; set; }
-
-    [SerializeField] private float maxTime;
     
+    [SerializeField] private float maxTime;
     [SerializeField] private Text resultText; // 매칭 시도 횟수 텍스트
     private int matchingCount; // 매칭 시도 횟수를 저장하는 변수
     [SerializeField] private Text timeText;
-    [SerializeField] private GameObject endText;
-    [SerializeField] private Image timerBackground; // 타이머 배경 이미지
-    [SerializeField] private AudioClip speedAudioClip; // 긴박한 배경음악 클립
-    [SerializeField] private Color timerTextColor = Color.red;
-
-
-    private AudioSource audioSource;
     [SerializeField] private GameObject result_UI;
-
-    [SerializeField] private Animator Timer_Anim;
+    private AudioSource audioSource;
     [SerializeField] private AudioClip matchAudio;
-    [SerializeField] private AudioClip failAudio;
+    [SerializeField] private AudioClip bgm1; // 2024.04.16
+    
     public int CardCount { get; set; }
-    public bool isPlay;
+    [HideInInspector]public bool isPlay;
     private float timer = 0;
-
+    private bool isBgm1Played = false; //// 2024.04.16
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
         }
-
-        
         matchingCount = 0; // 매칭 시도 횟수 초기화
         isPlay = false;
         timer = maxTime;
         audioSource = GetComponent<AudioSource>();
-        timeText.color = Color.white; // 초기 색상 설정
     }
-
     private void Start()
     {
         Time.timeScale = 1f;
-    }
 
+        timeText.color = Color.white;
+    }
     void Update()
     {
-        //시간경과 > 타이머 숫자 감소
+        
         timer -= Time.deltaTime;
-        //타이머값 > ui 표시
         timeText.text = $"{timer:F2}";
-
-
-
-        // 5초 이하일 때
-        if (timer <= 5.1f && timer > 5f)
-
+        if ( !isBgm1Played && timer > 0.1f && timer <= 5f  )  // 2024.04.16
         {
-            //텍스트색 붉게 변경[1f=red 0.5f=green/blue]
-            timeText.color = timerTextColor;
-
-            AudioManager.Instance.ChangeBGM(speedAudioClip);
-
+            AudioManager.instance.SwitchBGM(bgm1); // 2024.04.16
+            isBgm1Played = true;
+            timeText.color = Color.red;
         }
-        else
-        {
-            // 타이머 이미지 배경색 원래대로 변경
-            timerBackground.color = Color.white;
-
-            Debug.LogError("player is null in GameManager.Update()");
-
-            //배경음악 중지
-            audioSource.Stop();
-        }
-
-        if (timer <= 0)
+        if (timer <= 0 )
         {
             timer = 0;
             GameEnd();
-            
         }
     }
-
     private void GameEnd()
     {
         // 매칭 시도 횟수 text 오브젝트에 저장
@@ -99,7 +64,6 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 0f;
         result_UI.SetActive(true);
     }
-
     public void Matched()
     {
         // 같은 카드라면
@@ -109,7 +73,6 @@ public class GameManager : MonoBehaviour
             FirstCard.DestroyCard();
             SecondCard.DestroyCard();
             CardCount -= 2;
-            
             if (CardCount == 0)
             {
                 GameEnd();
@@ -117,13 +80,9 @@ public class GameManager : MonoBehaviour
         }
         else // 같지 않다면
         {
-            audioSource.PlayOneShot(failAudio);
             FirstCard.CloseCard();
             SecondCard.CloseCard();
-            Timer_Anim.SetTrigger("Fail");
-            timer -= 2;
         }
-        
         matchingCount++; // 매칭 시도 횟수 ++
         FirstCard = null;
         SecondCard = null;
