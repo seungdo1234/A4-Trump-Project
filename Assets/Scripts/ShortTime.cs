ca5175504579254e3c67bcfef20c1462c268ae27
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-04-18 23:43:39 +0900

    주석 처리

--- Assets/Scripts/ShortTime.cs
+++ Assets/Scripts/ShortTime.cs
@@ -16,8 +16,9 @@ public class ShortTime : MonoBehaviour
     private void Start()
     {
 
+        // 해당 난이도의 최단 시간 정보를 불러옴
         float sT = PlayerPrefs.GetFloat(dif.difficulty + "ShortTime");
-        // object shortTime = sT != 0 ? sT : "?"; 박싱 언박싱 위험 발생 !
+        // 만약 해당 난이도에 저장된 최단 시간이 없다면 ?, 있다면 소수점 2자리까지 문자열로 변환 후 저장
         string shortTime = sT != 0 ? sT.ToString("F2") : "?";
 
         // 난이도 마다 컬러를 설정해줌
@@ -26,9 +27,10 @@ public class ShortTime : MonoBehaviour
             Difficulty.Easy => "yellow",
             Difficulty.Normal => "aqua",
             Difficulty.Hard => "red",
-            _ => throw new ArgumentException($"난이도 설정이 잘못됐습니다 !") // default
+            _ => throw new ArgumentException($"난이도 설정이 잘못됐습니다 !")// 예외 처리 
         };
         
+        // 텍스트에 저장
         shortTimeText.text = $"<color={color}>{dif.difficulty}</color> 기록 {shortTime} 초";
 
     }
