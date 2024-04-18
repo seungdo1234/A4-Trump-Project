db7d5d5bfbbc415eeeeea64a8249765db226d4f4
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-04-18 19:51:07 +0900

    주석 부탁드립니다.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -10,35 +10,15 @@ public class ScoreManager : MonoBehaviour //2024.04.18 �����
     public Text endText;
     public GameManager gm;
     private DifficultyManager dif;
-    int cardCount = 0;
     
     private void Start()
     {
         gm = GameManager.instance;
-        dif = DifficultyManager.instance;
-        
-        switch (dif.difficulty)
-        {
-            case Difficulty.Easy:
-                cardCount = 8;
-                break;
-            case Difficulty.Normal:
-                cardCount = 16;
-                break;
-            case Difficulty.Hard:
-                cardCount = 24;
-                break;
-            default:
-                cardCount = 8;
-                break;
-        }
     }
     
     public void UpdateScoreText()
     {
-        Debug.Log("ddd");
         float timePercentage = gm.timer / gm.maxTime * 100f;
-        Debug.Log("ddd");
         int score = CalculateScore(timePercentage);
         endText.text = $"결과 : {score}";
     }
@@ -47,14 +27,12 @@ public class ScoreManager : MonoBehaviour //2024.04.18 �����
     {
         int Timescore = Mathf.Max(0, Mathf.RoundToInt(50f - (50f * (100f - timePercentage) / 100f)));
         
-        Debug.Log("ddd");
         float cardPercentage;
         float cardScore = 0f;
         int score = 0;
        
         int matchingCount = gm.matchingCount;
-
-        Debug.Log("ddd");
+        
         
         if (matchingCount == 0)
         {
@@ -62,29 +40,28 @@ public class ScoreManager : MonoBehaviour //2024.04.18 �����
         }
         else
         {
-            cardPercentage = ((float)(gm.CardCount) / cardCount) * 100f;
-            Debug.Log("ddd");
-            if (matchingCount >= 0 && matchingCount <= cardCount) // 0~8
+            cardPercentage = ((float)(gm.CardCount) / gm.CardCount) * 100f;
+            
+            if (matchingCount >= 0 && matchingCount <= gm.CardCount) // 0~8
             {
                 cardScore = 50f; // 50��
             }
-            else if (matchingCount > cardCount && matchingCount <= cardCount * 1.5f) // 9~12
+            else if (matchingCount > gm.CardCount && matchingCount <= gm.CardCount * 1.5f) // 9~12
             {
                 cardScore = 40f; // 40��
             }
-            else if (matchingCount > cardCount * 1.5f && matchingCount <= cardCount * 2) // 13~16
+            else if (matchingCount > gm.CardCount * 1.5f && matchingCount <= gm.CardCount * 2) // 13~16
             {
                 cardScore = 30f; // 30��
             }
-            else if (matchingCount > cardCount * 2 && matchingCount <= cardCount * 2.5f) // 17~20
+            else if (matchingCount > gm.CardCount * 2 && matchingCount <= gm.CardCount * 2.5f) // 17~20
             {
                 cardScore = 20f; // 20��
             }
-            else if (matchingCount > cardCount * 2.5f) // 21~
+            else if (matchingCount > gm.CardCount * 2.5f) // 21~
             {
                 cardScore = 10f; // 10��
             }
-            Debug.Log("ddd");
         }
 
         Debug.Log("TimeScore : " + Timescore);
@@ -92,7 +69,7 @@ public class ScoreManager : MonoBehaviour //2024.04.18 �����
         Debug.Log("score : " + score);
         Debug.Log("matchingCount : " + matchingCount);
         score = Timescore + (int)cardScore;
-        Debug.Log("ddd");
+
         return score;
     }
 }
